34a42c2aa2460e0f38366056d923351731f4a04a
chclock <chwhu009@gmail.com>
2018-02-07 14:16:25 +0800

    modify

--- simple-socket/libsocket/socket.c
+++ simple-socket/libsocket/socket.c
@@ -1,4 +1,4 @@
-#ifdef WIN32
+#ifdef __CYGWIN__
 #include <winsock2.h>
 #include <winsock.h>
 #include <ws2tcpip.h>
@@ -18,9 +18,9 @@
 
 int _socket(int domain, int type, int protocol)
 {
-#ifdef WIN32
+#ifdef __CYGWIN__
   WSADATA wsadata;
-  if (WSAStartup(MAKEWORD(1, 1), &wsadata) == SOCKET_ERROR)
+  if (WSAStartup(MAKEWORD(2, 2), &wsadata) == SOCKET_ERROR)
   {
     printf("WSAStartup() fail\n");
     exit(0);
@@ -46,10 +46,9 @@ int _listen(int socket, int backlog)
 
 int _accept(int socket)
 {
-  struct sockaddr_un sun;
-  int length;
-  length = sizeof(sun.sun_family) + sizeof(sun.sun_path);
-  return accept(socket, &sun, &length);
+  struct sockaddr_in clnt_addr;
+  socklen_t clnt_addr_size = sizeof(clnt_addr);
+  return accept(socket, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
 }
 
 int _connect(int socket, int family, char *addr, int port)
@@ -70,7 +69,7 @@ int _write(int fd, char *buf, ssize_t start, ssize_t n)
   m = n;
   while (m > 0)
   {
-#ifdef WIN32
+#ifdef __CYGWIN__
     if ((i = send(fd, buf, m, 0)) < 0)
     {
 #else
@@ -97,7 +96,7 @@ int _read(int fd, char *buf, size_t start, size_t n)
   buf += start;
   for (;;)
   {
-#ifdef WIN32
+#ifdef __CYGWIN__
     i = recv(fd, buf, n, 0);
 #else
     i = read(fd, buf, n);
@@ -111,18 +110,24 @@ int _read(int fd, char *buf, size_t start, size_t n)
 
 int _close(int fd)
 {
+#ifdef __CYGWIN__
+  return closesocket(fd);
+#else
   return close(fd);
+#endif
 }
 
-int _shutdown(int socket, int how){
-  return shutdown(socket,how);
+int _shutdown(int socket, int how)
+{
+  return shutdown(socket, how);
 }
 
-int _cleanup(){
+int _cleanup()
+{
   int i = 0;
-  #ifdef WIN32
-    i = WSACleanup();
-  #endif
+#ifdef __CYGWIN__
+  i = WSACleanup();
+#endif
   return i;
 }
 
@@ -132,121 +137,3 @@ char *get_error(void)
   extern int errno;
   return strerror(errno);
 }
-
-// size_t _strlen(const char *s)
-// {
-//   return strlen(s);
-// }
-
-// ssize_t _write_all(int fd, const char *buf, size_t nbyte)
-// {
-//   ssize_t i, m;
-//   m = nbyte;
-//   while (m > 0)
-//   {
-// #ifdef WIN32
-//     if ((i = send(fd, buf, m, 0)) < 0)
-//     {
-// #else
-//     if ((i = write(fd, buf, m)) < 0)
-//     {
-// #endif
-//       if (errno != EAGAIN && errno != EINTR)
-//         return i;
-//     }
-//     else
-//     {
-//       m -= i;
-//       buf += i;
-//     }
-//   }
-//   return nbyte;
-// }
-
-// FILE *_fdopen(int fildes, const char *mode)
-// {
-//   return fdopen(fildes, mode);
-// }
-
-// int _getpeername(int socket, struct sockaddr *address,
-//                  socklen_t *address_len)
-// {
-//   return getpeername(socket, address, address_len);
-// }
-
-// int _getsockname(int socket, struct sockaddr *address,
-//                  socklen_t *address_len)
-// {
-//   return getsockname(socket, address, address_len);
-// }
-
-// int _getsockopt(int socket, int level, int option_name,
-//                 void *option_value, socklen_t *option_len)
-// {
-//   return getsockopt(socket, level, option_name, option_value, option_len);
-// }
-
-// ssize_t _recv(int socket, void *buffer, size_t length, int flags)
-// {
-//   return recv(socket, buffer, length, flags);
-// }
-
-// ssize_t _recvfrom(int socket, void *buffer, size_t length,
-//                   int flags, struct sockaddr *address, socklen_t *address_len)
-// {
-//   return recvfrom(socket, buffer, length,
-//                   flags, address, address_len);
-// }
-
-// ssize_t _recvmsg(int socket, struct msghdr *message, int flags)
-// {
-// #ifdef WIN32
-
-// #else
-//   return recvmsg(socket, message, flags);
-// #endif
-// }
-
-// ssize_t _send(int socket, const void *message, size_t length, int flags)
-// {
-//   return send(socket, message, length, flags);
-// }
-
-// ssize_t _sendmsg(int socket, const struct msghdr *message, int flags)
-// {
-// #ifdef WIN32
-// #else
-//   return sendmsg(socket, message, flags);
-// #endif
-// }
-
-// ssize_t _sendto(int socket, const void *message, size_t length, int flags,
-//                 const struct sockaddr *dest_addr, socklen_t dest_len)
-// {
-//   return sendto(socket, message, length, flags,
-//                 dest_addr, dest_len);
-// }
-
-// int _setsockopt(int socket, int level, int option_name,
-//                 const void *option_value, socklen_t option_len)
-// {
-//   return setsockopt(socket, level, option_name,
-//                     option_value, option_len);
-// }
-
-// int _shutdown(int socket, int how)
-// {
-// #ifdef WIN32
-//   WSACleanup();
-// #endif
-//   return shutdown(socket, how);
-// }
-
-// int _socketpair(int domain, int type, int protocol,
-//                 int socket_vector[2])
-// {
-// #ifdef WIN32
-// #else
-//   return socketpair(domain, type, protocol, socket_vector);
-// #endif
-// }
